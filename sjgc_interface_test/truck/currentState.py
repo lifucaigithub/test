1aadb0bff0139fd0f3f8da7b91622696c82bf6f3
lifucai <lifucai@g7.com.cn>
2018-01-10 17:46:59 +0800

    all

--- sjgc_interface_test/truck/currentState.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Elvis'
-#coding:utf-8
-from interface_test_utils import utils
-
-if __name__ == '__main__':
-    u = utils.utils()
-    #登录
-    u.get(u.create_parameter('test', 'truck.truck.currentState', 'gpsnos=90000010,11001176', 'smart029'))
